84950fbc4ad8c967c44ac6e6604c2a191dc82501
mcollins12321 <mcollins12321@gmail.com>
2015-10-01 13:01:27 -0400

    Update Deck_Test.java

--- SpringBorgata/src/test/java/pokerBase/Deck_Test.java
+++ SpringBorgata/src/test/java/pokerBase/Deck_Test.java
@@ -8,10 +8,14 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-public class Deck_Test {
+import pokerBase.Deck;
+import pokerEnums.*; 
 
+public class Deck_Test {
+	private static Deck deck; 
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
+		deck = new Deck(); 
 	}
 
 	@AfterClass
@@ -29,9 +33,34 @@ public class Deck_Test {
 	@Test
 	public void TestFullDeck() {
 		//	I put this test in so it would pass...
-		//TODO: Code a unit test to make sure calling the deck constructor passes back 52 cards in a deck.
+		//TODO: Code a unit test to make sure calling the deck constructor passes back 52 cards in a deeck.
 		
-		assertTrue(1==1);
-	}
+		assertTrue(deck.getTotalCards() == 52); 
+		int diamonds = 0; 
+		int hearts = 0; 
+		int spades = 0; 
+		int clubs = 0; 
+		int rank = 0; 
+		for (Card c : deck.getCards()) { 
+			switch (c.getSuit()) { 
+				case DIAMONDS:
+					diamonds++; 
+					break; 
+				case HEARTS: 
+					hearts++; 
+					break; 
+				case SPADES: 
+					spades++; 
+					break;
+				case CLUBS: 
+					clubs++; 
+					break; 
+			}
+			rank += c.getRank().getRank();
+		}
+		assertTrue(diamonds == hearts && hearts == spades && spades == clubs); 
+		assertTrue(diamonds == 13); 
+		assertTrue(rank == 416); // A sum of all the card ranks. 
 
+	}
 }
